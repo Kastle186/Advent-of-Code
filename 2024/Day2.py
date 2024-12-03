66d398014d66e76ae8d5555402d8bd454d687c3c
Ivan Kastle <ivankastle@Ivans-MacBook-Air.local>
2024-12-03 00:44:49 -0800

    Started solving Day 2 part 2. It is a big mess but hopefully it will work in the end.

--- 2024/Day2.py
+++ 2024/Day2.py
@@ -4,6 +4,7 @@ from enum import Enum
 import sys
 
 class Operation(Enum):
+    NONE = 0
     INCREASE = 1
     DECREASE = 2
 
@@ -16,7 +17,8 @@ def is_safe(report: list[int]) -> bool:
     if len(report) != len(set(report)) or abs(report[0] - report[1]) > 3:
         return False
 
-    series_op = None
+    series_op = Operation.NONE
+
     if report[0] < report[1]:
         series_op = Operation.INCREASE
     else:
@@ -34,8 +36,60 @@ def is_safe(report: list[int]) -> bool:
 
 
 def is_safe_dampened(report: list[int]) -> bool:
-    if is_safe(report):
-        return True
+    series_op = Operation.NONE
+    has_forgiven = False
+
+    for i in range(0, len(report) - 1):
+        diff = report[i] - report[i + 1]
+
+        if abs(diff) > 3:
+            # We've already removed one item to make it safe, so another faulty one
+            # means this sequence is not safe.
+            if has_forgiven:
+                return False
+
+            # Only the last number yields a bigger difference, so we can remove it
+            # and consider this a safe sequence.
+            if i == len(report) - 2:
+                return True
+
+            diff_1_3 = report[i] - report[i + 2]
+
+            if series_op == Operation.NONE and diff_1_3 > 0 and abs(diff_1_3) <= 3:
+                series_op = Operation.DECREASE
+            elif series_op == Operation.NONE and diff_1_3 < 0 and abs(diff_1_3) <= 3:
+                series_op = Operation.INCREASE
+
+            if series_op == Operation.NONE or abs(diff_1_3) > 3:
+                diff_2_3 = report[i + 1] - report[i + 2]
+
+                if series_op == Operation.NONE and diff_2_3 > 0 and abs(diff_2_3) <= 3:
+                    series_op = Operation.DECREASE
+                elif series_op == Operation.NONE and diff_2_3 < 0 and abs(diff_2_3) <= 3:
+                    series_op = Operation.INCREASE
+
+                if series_op == Operation.NONE or abs(diff_2_3) > 3:
+                    return False
+
+                if (diff_2_3 > 0 and series_op == Operation.INCREASE) \
+                   or (diff_2_3 < 0 and series_op == Operation.DECREASE):
+                    return False
+
+                i += 1
+                has_forgiven = True
+                continue
+
+            if (diff_1_3 > 0 and series_op == Operation.INCREASE) \
+               or (diff_1_3 < 0 and series_op == Operation.DECREASE):
+                return False
+
+            i += 2
+            has_forgiven = True
+            continue
+
+        if diff == 0:
+            if has_forgiven:
+                return False
 
     return True
 
