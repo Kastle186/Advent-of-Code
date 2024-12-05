6c4232dac6855ddba6ac011dc557ef26dc59030e
Ivan Kastle <ivankastle@Ivans-MacBook-Air.local>
2024-12-05 02:39:15 -0800

    Began Day 5 and updated .gitignore with MacOS's .DS_Store file.

---
+++ 2024/Day5.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/env lua
+
+-- MAIN FUNCTION!
+
+function main()
+   local pages_conditions = {}
+   local input_file = io.open(arg[1], "r")
+
+   for line in io.lines(input_file) do
+   end
+end
+
+-- HELPER FUNCTIONS!
+
+-- RUN THE MAIN FUNCTION!
+
+main()
