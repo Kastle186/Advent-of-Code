e03b1d913604256f67635788c36c1d7ad0bacde4
Ivan Kastle <ivankastle@Ivans-MacBook-Air.local>
2024-12-10 02:19:26 -0800

    A bit of progress on Day 9 before passing out.

--- 2024/Day9.mjs
+++ 2024/Day9.mjs
@@ -3,22 +3,47 @@
 import fs from 'node:fs';
 
 function main() {
+    /* SETUP! */
+
     const inputDisk = fs.readFileSync(process.argv[2], 'utf8').trim();
-    console.log("INPUT: %s", inputDisk);
-    const diskLayout = diskMapToLayout(inputDisk);
-    console.log("LAYOUT: %s", diskLayout.join(''));
+
+    // We have to know the sizes (number of blocks) of each file, as well as the
+    // size of each one of the free space segments between them to later implement
+    // the defragmentation/compression algorithm.
+    const {fileBlocks, spaceBlocks} = separateDiskMapContents(inputDisk);
+
+    // Next, we build an array of tuples representing how many times each file ID
+    // appears in each segment of the compressed disk image.
 }
 
 /* HELPER FUNCTIONS! */
 
-// NOTE: We will have to get more creative with data structures for the real input,
-//       as processing digits alone will only lead us so far as to id=9 (i.e. 10 files).
-//       And the puzzle input is much, much larger than that.
+function separateDiskMapContents(diskMap) {
+    const fileBlocks = [];
+    const spaceBlocks = [];
+
+    // We know that the number of blocks of each file and each segment of contiguous
+    // space are alternated in the disk map. So, if we're starting with file blocks,
+    // and arrays are 0-index-based, then all index pair numbers are file blocks and
+    // all odd numbers are space blocks.
+
+    for (let index in diskMap) {
+        const numBlocks = diskMap[index];
+        index % 2 == 0 ? fileBlocks.push(numBlocks) : spaceBlocks.push(numBlocks);
+    }
+
+    return {
+        fileBlocks: fileBlocks,
+        spaceBlocks: spaceBlocks,
+    }
+}
+
+/* DRAFT FUNCTIONS! */
 
 function diskMapToLayout(diskMap) {
     let isFile = true;
     let fileId = 0;
-    const layout = []
+    const layout = [];
 
     // Each digit of the map indicates how many blocks of memory the item takes.
     // 0 and even numbers refer to files, and uneven numbers refer to free space.
